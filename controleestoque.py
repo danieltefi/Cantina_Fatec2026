7a08fe4ab31d382ad05354a2b93ba41e542c1818
Daniel <daniel.odontolegal@gmail.com>
2026-03-22 11:46:00 -0300

    implementa o controle de estoque
    cadastro de produtos, gerenciamento e edição de quantidade

---
+++ controleestoque.py
@@ -0,0 +1,36 @@
+class Produto: # classe para gerenciar produtos
+    def __init__(self, nome, preco_compra, preco_venda, quantidade, data_compra, data_vencimento):
+        self.nome = nome.capitalize()
+        self.preco_compra = float(preco_compra)
+        self.preco_venda = float(preco_venda)
+        self.quantidade = int(quantidade)
+        self.data_compra = data_compra
+        self.data_vencimento = data_vencimento
+
+class GerenciarEstoque: # gerenciar estoque total
+    def __init__(self): # o primeiro da lista (índice 0) será sempre o mais velho
+        self.lista_produtos = []
+
+    def adicionar_produto(self, produto):
+        self.lista_produtos.append(produto)
+        print(f'Produto {produto.nome} adicionado com sucesso!')
+
+    def mostrar_estoque(self):
+        if not self.lista_produtos:
+            print('Sem produtos em estoque!')
+            return
+
+        print('Estoque de produtos')
+        for p in self.lista_produtos:
+            print(f'Produto: {p.nome}, \nQtd: {p.quantidade}, \nCompra: {p.data_compra}, \nVencimento: {p.data_vencimento}')
+
+    def editar_quantidade(self, nome_busca, nova_qtd): # permite editar a quantidade
+        encontrado = False
+        for p in self.lista_produtos:
+            if p.nome == nome_busca.capitalize():
+                p.quantidade = int(nova_qtd)
+                encontrado = True
+                print(f'Quantidade de {p.nome} atualizada para {nova_qtd}.')
+                break
+        if not encontrado:
+            print('Produto não encontrado para edição.')
